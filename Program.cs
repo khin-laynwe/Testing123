6eb9803dd30267f505085ca9bc3b7bbddab3e607
khin-laynwe <76720938+khin-laynwe@users.noreply.github.com>
2021-01-17 21:25:08 +0630

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine("Integer Array:");
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            Console.WriteLine();
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine("Boolean Array:");
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            Console.WriteLine();
+
+            var names = new string[3] { "Mary", "John", "Jim" };
+            Console.WriteLine("String Array:");
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+        }
+    }
+}
